fff8f67a223e756ca11e3f2ab05d9ae00b5c2f91
randomGuy8-8 <108034246+randomGuy8-8@users.noreply.github.com>
2024-02-11 19:57:10 -0400

    Add project files.

---
+++ Pubs.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Pubs.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Pubs.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Pubs.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
